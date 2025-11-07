af9dcf2626f00ee7ea26db63e428b174ccef7008
peterbitar <bitarpeter@gmail.com>
2025-11-07 09:40:42 -0500

    Initial Commit

---
+++ Testing Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Testing Project
+//
+//  Created by Peter on 2025-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Testing Project/Testing_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_ProjectApp.swift
+//  Testing Project
+//
+//  Created by Peter on 2025-11-07.
+//
+
+import SwiftUI
+
+@main
+struct Testing_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
